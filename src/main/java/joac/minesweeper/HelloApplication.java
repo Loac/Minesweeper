715a5577a2fe4d7d39101d6bc1ac15bd13f507ad
Dmitry Loac <reloac@gmail.com>
2023-07-10 06:20:37 +0300

    gui

--- src/main/java/joac/minesweeper/HelloApplication.java
+++ src/main/java/joac/minesweeper/HelloApplication.java
@@ -18,7 +18,7 @@ import java.util.concurrent.TimeUnit;
 
 public class HelloApplication extends Application {
 
-    private final Engine engine = new Engine();
+    private final Engine engine;
 
     private Stage stage;
 
@@ -26,22 +26,46 @@ public class HelloApplication extends Application {
 
     private Board board;
 
-    private final TopGridPane grid = new TopGridPane();
+    private final VBox boardContainer;
 
-    private final TimerLabel timer = new TimerLabel();
+    private final Scene scene;
 
-    private final StateLabel state = new StateLabel();
+    private final TimerLabel timer;
 
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final StateLabel state;
+
+    private final ScheduledExecutorService scheduler;
 
     /**
      * Основные элементы интерфейса вынесены в свойства класса.
      * Цикл, периодически проверяет состояние игры и выводит данные в интерфейс.
+     *
+     * <p> Компонент boardContainer содержит игровое поле, которое создается заново для каждой новой игры.
+     * Он является своего рода ссылкой на конкретный элемент сцены, к которому можно получить доступ
+     * напрямую, минуя всех родителей.
      */
     public HelloApplication() {
+        // Game.
+        engine = new Engine();
+
+        // GUI.
+        timer = new TimerLabel();
+        state = new StateLabel();
+
+        TopGridPane grid = new TopGridPane();
         grid.setState(state);
         grid.setTimer(timer);
 
+        boardContainer = new VBox();
+        VBox root = new VBox();
+        root.getChildren().addAll(buildApplicationMenu(), grid, boardContainer);
+
+        scene = new Scene(root);
+        String css = Objects.requireNonNull(this.getClass().getResource("application.css")).toExternalForm();
+        scene.getStylesheets().add(css);
+
+        // Service.
+        scheduler = Executors.newScheduledThreadPool(1);
         scheduler.scheduleWithFixedDelay(() ->
                 Platform.runLater(() -> {
                     if (null == game)
@@ -70,29 +94,21 @@ public class HelloApplication extends Application {
 
         board = new Board(game.getField());
         board.setHandler(this::actionMinefield);
-
-        VBox root = new VBox();
-        root.getChildren().addAll(buildApplicationMenu(), grid, board);
-        root.autosize();
-
-        Scene scene = new Scene(root, root.getBoundsInParent().getWidth(), root.getBoundsInParent().getHeight());
-        String css = Objects.requireNonNull(this.getClass().getResource("application.css")).toExternalForm();
-        scene.getStylesheets().add(css);
+        boardContainer.getChildren().clear();
+        boardContainer.getChildren().add(board);
+        stage.sizeToScene();
 
         return scene;
     }
 
-    public BorderPane buildApplicationMenu() {
+    public AppMenu buildApplicationMenu() {
         AppMenu appMenu = new AppMenu();
         appMenu.getMainMenu().setOnNewEasyGameAction(event -> actionNewGame(GameProperties.easy()));
         appMenu.getMainMenu().setOnNewMediumGameAction(event -> actionNewGame(GameProperties.medium()));
         appMenu.getMainMenu().setOnNewHardGameAction(event -> actionNewGame(GameProperties.hard()));
         appMenu.getMainMenu().setOnCloseAction(event -> actionExit());
 
-        BorderPane borderPane = new BorderPane();
-        borderPane.setTop(appMenu);
-
-        return borderPane;
+        return appMenu;
     }
 
     public void actionNewGame(GameProperties properties) {
